22edc41414465f18adfb7e63c52f0677c07f33a4
tenar <ya_nastusha@mail.ru>
2020-03-27 23:55:39 +0300

    tenar

--- terms/output.py
+++ terms/output.py
@@ -1,6 +1,13 @@
 #1 Задача "Сумма трех чисел"
 
-a = int(input())
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(a + b + c)
+
+#2 Задача "Площадь прямоугольного треугольника
+
+h = int(input())
 b = int(input())
-c = int(input())
-print(a + b + c)
+s = 1/2 * b * h
+print(s)
