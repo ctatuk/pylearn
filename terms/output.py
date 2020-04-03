a11a984f726ce5c2a6fe1e731e0f32fc4506ac98
tenar <ya_nastusha@mail.ru>
2020-04-03 12:15:52 +0300

    ход ферзя

--- terms/output.py
+++ terms/output.py
@@ -7,7 +7,20 @@
 
 #2 Задача "Площадь прямоугольного треугольника
 
-h = int(input())
-b = int(input())
-s = 1/2 * b * h
-print(s)
+#h = int(input())
+#b = int(input())
+#s = 1/2 * b * h
+#print(s)
+
+#3 задача ход ферзя
+
+def compare_chess_queen(x1, y1, x2, y2):
+    if x1 == x2 or y1 == y2:
+        print('Yes')
+    elif abs(x2 - x1) == abs(y2 - y1):
+        print('Yes')
+    else:
+        print('No')
+compare_chess_queen(7, 5, 1, 1)
+
+
