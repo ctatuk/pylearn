81891bdd64c2d6bd6dd5d429587dcfca1101155a
Tenar84 <ya_nastusha@mail.ru>
2020-05-04 21:07:54 +0300

    sum of three numbers

---
+++ python_tutor/01_io/sumnumbers.py
@@ -0,0 +1,9 @@
+def sum_numbers(a, b, c):
+    '''
+    Source: https://pythontutor.ru/lessons/inout_and_arithmetic_operations/problems/aplusbplusc/
+    Condition
+    Write a program that reads three numbers and displays their sum.
+    Each number is written on a separate line.
+    '''
+    print(a + b + c)
+sum_numbers(2, 3, 6)
