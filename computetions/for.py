8e095a956f2d4b81e56c5b3e69b41dfb9543b5ce
camapa87 <ya_nastusha@mail.ru>
2020-05-10 21:20:27 +0300

    Factorial, number of zero and stair (for)

--- computetions/for.py
+++ computetions/for.py
@@ -1,43 +1,45 @@
-'''def rainbow():
-i = 1
-for color in 'red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet':
-    print('#', i, ' color of rainbow is ', color, sep = '')
-    i += 1'''
-
-#Условие
-#Факториалом числа n называется произведение 1 × 2 × ... × n.Обозначение: n!.
-#По данному натуральному n вычислите значение n!.Пользоваться математической
-#библиотекой math в этой задаче запрещено.
-
-"""def factorial(num):
+'''
+    Source: http://pythontutor.ru/lessons/for_loop/problems/factorial/
+    Condition
+    The factorial of n is the product 1 × 2 × ... × n. Designation: n !.
+    For a given positive integer n, calculate the value of n !.
+    Using the math math library in this task is prohibited.
+'''
+def factorial(num):
     fact = 1
     for i in range(1, num):
         fact = fact*i
         print(fact)
-factorial(10)"""
+factorial(10)
 
-
-#Дано N чисел: сначала вводится число N, затем вводится ровно N целых чисел.
-#Подсчитайте количество нулей среди введенных чисел и выведите это количество.
-#Вам нужно подсчитать количество чисел, равных нулю, а не количество цифр.
-
+'''
+    Source: http://pythontutor.ru/lessons/for_loop/problems/how_many_zeroes/
+    Condition
+    Given N numbers: first enter the number N, 
+    then enter exactly N integers.
+     Count the number of zeros among the entered numbers and print this number. 
+    You need to count the number of numbers equal to zero,
+    not the number of digits.
+'''
 def zero_counter(num_list):
     counter = 0
     num = len(num_list)
     indices = range(num)
-    """print(indices)
-    print(list(indices))
-    zip_list = list(zip(num_list, indices))
-    print(zip_list)"""
+    #print(indices)
+    #print(list(indices))
+    #zip_list = list(zip(num_list, indices))
+    #print(zip_list)
     for i in zip(num_list, indices):
         if i[0] == 0:
             counter += 1
             print(i)
 zero_counter([1, 0, 10, 30, 0, 7, 25, 0, 75])
 
-
-#По данному натуральному n ≤ 9 выведите лесенку из n ступенек,
-# i-я ступенька состоит из чисел от 1 до i без пробелов.
+'''
+    Source: http://pythontutor.ru/lessons/for_loop/problems/ladder/
+    Condition
+    Given a positive integer n ≤ 9, output a ladder from n steps,
+    the i-th step consists of numbers from 1 to i without spaces.
 '''
 def stair(num):
     if num <= 9:
@@ -45,4 +47,4 @@ def stair(num):
         for i in range(1, num):
             s = s + str(i)
             print(s)
-stair(7)'''
+stair(7)
