ce7ef0e2824cb81bd6438ce17a0d644716fd9902
camapa87 <ya_nastusha@mail.ru>
2020-05-10 21:28:13 +0300

    hypotenuse (computation)

--- computetions/гипотенуза.py
+++ computetions/гипотенуза.py
@@ -1,9 +1,15 @@
-"""Условие
-
-Дано два числа a и b. Выведите гипотенузу треугольника с заданными катетами.
+"""
+    Source: http://pythontutor.ru/lessons/int_and_float/problems/hypo
+    Condition
+    Two numbers a and b are given. Derive the hypotenuse of a triangle with given legs.
 """
 import math
 def find_hypotenuse(katet_a, katet_b):
     hypo_c = math.sqrt(katet_a ** 2 + katet_b ** 2)
     print(hypo_c)
 find_hypotenuse(1, 1)
+
+import math
+def find_hypotenuse(katet_a, katet_b):
+    return math.hypot(katet_a, katet_b)
+print(find_hypotenuse(3, 4))
