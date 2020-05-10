2e0bc0a47c6f8c86a1f54704fd02af00aec4a91b
camapa87 <ya_nastusha@mail.ru>
2020-05-10 21:05:43 +0300

    ranks (for)

--- computetions/ranks.py
+++ computetions/ranks.py
@@ -1,11 +1,15 @@
-# Ряды - 1
 '''
-Даны два целых числа A и B (при этом A ≤ B).
-Выведите все числа от A до B включительно.
+    Source: http://pythontutor.ru/lessons/for_loop/problems/series_1/
+    Condition
+    Two integers A and B are given (with A ≤ B).
+    Print all numbers from A to B inclusive.
 '''
 def rank_one(a, b):
-    a <= b
+    if a > b:
+        print('a должно быть меньше или равно b')
+        return
+    num_list = []
     for i in range(a, b + 1):
-        i <= 1
-        print(i)
+        num_list.append(i)
+    print(num_list)
 rank_one(-3, 14)
